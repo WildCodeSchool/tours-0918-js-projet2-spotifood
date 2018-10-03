1f4056e63d434eeda2817e50af5dac675cf0c6ca
Emilie Huet <hemilie94@gmail.com>
2018-10-03 11:02:53 +0200

    import NgbModule

--- spotifood/src/app/app.module.ts
+++ spotifood/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { CarrouselComponent } from './carrousel/carrousel.component';
@@ -7,6 +8,7 @@ import { CarrouselComponent } from './carrousel/carrousel.component';
 @NgModule({
   declarations: [
     AppComponent,
+    NgbModule,
     CarrouselComponent
   ],
   imports: [

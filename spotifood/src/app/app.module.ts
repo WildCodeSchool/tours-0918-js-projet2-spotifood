9fafdee599123e50cba0219c0309ad4e11345dcc
Emilie Huet <hemilie94@gmail.com>
2018-10-03 11:01:36 +0200

    add import bootstrap in sttles.css + generate CarrouselComponent

--- spotifood/src/app/app.module.ts
+++ spotifood/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarrouselComponent } from './carrousel/carrousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarrouselComponent
   ],
   imports: [
     BrowserModule

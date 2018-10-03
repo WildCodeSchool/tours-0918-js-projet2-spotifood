99b8c9785361660d053fabed40961cabfcf33326
Loubna Bouam <bouamloubna5@gmail.com>
2018-10-03 22:01:38 +0200

    creation list produit + modal pour consulter le detail produit

--- spotifood/src/app/app.module.ts
+++ spotifood/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { ListProduitComponent } from './list-produit/list-produit.component';
+import { ModalProduitComponent } from './modal-produit/modal-produit.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListProduitComponent,
+    ModalProduitComponent,
+    PaginationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]

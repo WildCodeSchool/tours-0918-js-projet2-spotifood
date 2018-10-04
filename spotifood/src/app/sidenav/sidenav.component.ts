cf12d5f68bee843b34369ae59fedb0d5089fcf6b
Valentin Berruer <valb37@icloud.com>
2018-10-04 15:50:07 +0200

    sidenav

--- spotifood/src/app/sidenav/sidenav.component.ts
+++ spotifood/src/app/sidenav/sidenav.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sidenav',
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+	selector: 'app-sidenav',
+	templateUrl: './sidenav.component.html',
+	styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
+	bool: boolean;
 
 
-  constructor() { }
-
-  ngOnInit() {
-  	
-  } 
+	constructor() { 
+	}
+	showFilter(){
+		this.bool = !this.bool;
+	}
+	ngOnInit() {
+		this.bool = false;
+	} 
 }
 
 

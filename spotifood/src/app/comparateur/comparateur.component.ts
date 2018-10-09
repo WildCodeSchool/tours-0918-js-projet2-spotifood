17e26914a32d8d9072a1b8ad7be9f893def2d40e
Alex <alex.gauthier41@gmail.com>
2018-10-09 10:30:37 +0200

    MAJ Comparateur

--- spotifood/src/app/comparateur/comparateur.component.ts
+++ spotifood/src/app/comparateur/comparateur.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-comparateur',
   templateUrl: './comparateur.component.html',
   styleUrls: ['./comparateur.component.css']
 })
 export class ComparateurComponent implements OnInit {
-	images: string[];
+  page = 4;
 
 
   constructor() { }
 
   ngOnInit() {
-  	this.images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
   }
 }

ac7a67a7c755406a723d898438402bad2e822dc6
Alex <alex.gauthier41@gmail.com>
2018-10-12 15:15:43 +0200

    MAJ Comparateur

--- spotifood/src/app/common/common.service.ts
+++ spotifood/src/app/common/common.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
+import products from './products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
-
   products: any[];
 
   constructor() {
     if (!localStorage.products) {
 
-      this.products = [];
+      this.products = products;
+      this.saveToLocalStorage(this.products);
 
     } else {
       const data = JSON.parse(localStorage.products);
@@ -20,6 +21,15 @@ export class CommonService {
 
   get() {
       return this.products;
+
   }
 
+  /**
+   * Mise à jour du local storage avec la liste "todos"
+   * @param products
+   */
+  saveToLocalStorage(products) {
+    const data = JSON.stringify(products);
+    localStorage.setItem('products', data);
+  }
 }

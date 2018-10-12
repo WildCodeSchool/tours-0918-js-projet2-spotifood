1420d675169aef85b87c7358251366f6e0b4b741
Emilie Huet <hemilie94@gmail.com>
2018-10-12 11:13:02 +0200

    add routes and import trash image in assets

--- spotifood/src/app/app.routes.ts
+++ spotifood/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Route } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { ProduitsComponent } from './produits/produits.component';
 import { FormAdminEditComponent } from './form-admin-edit/form-admin-edit.component';
+import { ProduitAdminComponent } from './produit-admin/produit-admin.component';
 
 const ROUTES: Route[] = [
   {
@@ -22,6 +23,10 @@ const ROUTES: Route[] = [
   {
     path: 'edit/:id',
     component: FormAdminEditComponent
+  },
+  {
+    path: 'produits-admin',
+    component: ProduitAdminComponent
   }
 ];
 

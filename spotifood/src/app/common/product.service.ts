531e36e019de5ce8a00401a374b508789a91be88
maxdhn <maxduchan@gmail.com>
2018-10-23 16:33:14 +0200

    fix doc + remove gallery service

--- spotifood/src/app/common/product.service.ts
+++ spotifood/src/app/common/product.service.ts
@@ -17,7 +17,7 @@ export class ProductService {
   selectedAllergene: string;
   selectedPackaging: string;
 
-    constructor() {
+  constructor() {
     if (!localStorage.products) {
       // Initialisation du local storage et du tableau products avec tableau des produits
       this.products = products.map((x) => {
@@ -31,7 +31,7 @@ export class ProductService {
         product.labels = x.labels;
         product.categories = x.categories;
         product.packaging = x.packaging;
-        product.ingredients =  x.ingredients;
+        product.ingredients = x.ingredients;
         product.allergens = x.allergenes;
         product.nutriscore = x.nutriscore;
         product.images = x.image;
@@ -56,18 +56,26 @@ export class ProductService {
     }
   }
 
-  // save product(s) to localStorage
+  /**
+   * save product(s) to localStorage
+   * @param product Product to save
+   */
   saveToLocalStorage(product) {
     const data = JSON.stringify(product);
     localStorage.setItem('products', data);
   }
 
-  // get products
+  /**
+   * get all product
+   */
   get(): Product[] {
     return this.products;
   }
 
-  // create a unique identifier for the product, and store the product into the localStorage
+  /**
+   *create a unique identifier for the product, and store the product into the localStorage
+   * @param element
+   */
   add(element) {
     element.id = Date.now();
 
@@ -83,7 +91,10 @@ export class ProductService {
     this.saveToLocalStorage(this.products);
   }
 
-  // capitalise data inputs (for consistency)
+  /**
+   * capitalise data inputs (for consistency)
+   * @param str String to capitalize
+   */
   capitalize(str) {
     return str.split(',').map(w => {
       w = w.trim();
@@ -92,20 +103,29 @@ export class ProductService {
     }).join(',');
   }
 
-  // after an admin changed the information on a product, save the changes to localStorage
+  /**
+   * after an admin changed the information on a product, save the changes to localStorage
+   * @param element
+   */
   update(element) {
     const index = this.products.indexOf(element);
     this.products[index] = element;
     this.saveToLocalStorage(this.products);
   }
 
-  // find the product associated with a given id
+  /**
+   * find the product associated with a given id
+   * @param id Product id
+   */
   getProduct(id: string) {
     // tslint:disable-next-line:triple-equals
     return this.products.find(product => product.id == id);
   }
 
-  // delete the product associated with a given id
+  /**
+   * delete the product associated with a given id
+   * @param id Product id
+   */
   delete(id): void {
     const produit = this.products.find(product => product.id === id);
     const index = this.products.indexOf(produit);
@@ -113,23 +133,32 @@ export class ProductService {
     this.saveToLocalStorage(this.products);
   }
 
-  // get all products associated with a given category
+  /**
+   * get all products associated with a given category
+   * @param categorie Category name
+   */
   getProduitByCategorie(categorie): Product[] {
     const arrayTri = this.products.filter((product) => {
       if (product.categories && product.categories.includes(categorie)) {
-            return product;
+        return product;
       }
     });
-      return arrayTri;
+    return arrayTri;
   }
 
-  // get all products associated with a given brand
+  /**
+   * get all products associated with a given brand
+   * @param marque Brand name
+   */
   getProductByMarque(marque: string): Product[] {
     const arrayTri = this.products.filter((product) => product.brands === marque);
     return arrayTri;
   }
 
-  // get all products containing a given allergen
+  /**
+   * get all products containing a given allergen
+   * @param allergene
+   */
   getProductByAllergene(allergene: string) {
     const arrayTri = this.products.filter((product) => {
       if (product.allergens && product.allergens.includes(allergene)) {
@@ -139,7 +168,11 @@ export class ProductService {
     return arrayTri;
   }
 
-  // get all products associated with a given packaging
+  /**
+   * get all products associated with a given packaging
+   * @param packaging Packagin name
+   *
+   */
   getProductByPackaging(packaging: string) {
     const arrayTri = this.products.filter((product) => {
       if (product.packaging && product.packaging.includes(packaging)) {
@@ -149,67 +182,91 @@ export class ProductService {
     return arrayTri;
   }
 
-  // get all products associated with a given nutritional value
+  /**
+   * get all products associated with a given nutritional value
+   * @param score Score name
+   */
   getProductByScore(score: string) {
     const arrayTri = this.products.filter((product) => product.nutriscore === score);
     return arrayTri;
   }
 
-  // create a list of all existing categories (returns multidimensional array)
+  /**
+   * create a list of all existing categories (returns multidimensional array)
+   */
   getCategories(): string[] {
     this.categories = this.products.map((product) => {
-        const categorie: string [] = product.categories;
-        return categorie;
+      const categorie: string[] = product.categories;
+      return categorie;
     });
     return this.categories;
 
   }
 
-  // list all categories in a single array (returns a single array)
+  /**
+   * list all categories in a single array (returns a single array)
+   * @param categories
+   */
   tableauCategorie(categories) {
     categories = categories.join(',');
     categories = categories.split(',');
     return categories;
   }
 
-  // remove duplicates
+  /**
+   * remove duplicates
+   * @param categoriesTotal
+   */
   categorieUnique(categoriesTotal) {
     return categoriesTotal.sort().filter((item, pos, ary) => {
-    return !pos || item !== ary[pos - 1];
-  });
-}
+      return !pos || item !== ary[pos - 1];
+    });
+  }
 
-  // change selected category
+  /**
+   * change selected category
+   * @param newSelecetedCategorie
+   */
   changeSelectedCategorie(newSelecetedCategorie: string) {
     this.selectedCategorie = newSelecetedCategorie;
     return this.selectedCategorie;
   }
 
-  // change selected brand
+  /**
+   * change selected brand
+   * @param marque brand name
+   */
   changeSelectedMarque(marque: string) {
     this.selectedMarque = marque;
     return this.selectedMarque;
   }
 
-  // change selected allergen
+  /**
+   * change selected allergen
+   * @param allergene
+   */
   changeSelectedAllergene(allergene: string) {
     this.selectedAllergene = allergene;
     return this.selectedAllergene;
   }
 
-  // change selected packaging
+  /**
+   * change selected packaging
+   * @param packaging Packagin name
+   */
   changeSelectedPackaging(packaging: string) {
     this.selectedPackaging = packaging;
     return this.selectedPackaging;
   }
 
-
-
-  // display the product the user searched for
+  /**
+   * display the product the user searched for
+   * @param name Product name
+   */
   dispProduct(name): Product[] {
     const arrayTri = this.products.filter((product) => {
       if (product.name && product.name.includes(name)) {
-            return product;
+        return product;
       }
     });
     return arrayTri;
